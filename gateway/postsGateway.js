fb4b70dd1fee1af1224149889d545915348501df
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-03-25 14:43:53 +0200

    created controlled forms for editing posts

--- gateway/postsGateway.js
+++ gateway/postsGateway.js
@@ -1,9 +1,25 @@
 import axios from 'axios';
 
-export const getPostsList = async () =>{
-    return await axios.get('http://127.0.0.1:8000/api/posts');
+export const getPostsList = async () => {
+    const res = await axios.get('http://127.0.0.1:8000/api/posts');
+
+    return res.data;
 }
 
-export const createPost = async (post) =>{
-    return await axios.post('http://127.0.0.1:8000/api/posts', {text: post});
+export const createPost = async (post) => {
+    await axios.post('http://127.0.0.1:8000/api/posts', {text: post});
+
+    return await getPostsList();
+}
+
+export const deletePost = async (id) => {
+    await axios.delete(`http://127.0.0.1:8000/api/posts/${id}`);
+
+    return await getPostsList();
+}
+
+export const changePost = async (id, textPosts) => {
+    await axios.put(`http://127.0.0.1:8000/api/posts/${id}`, {text: textPosts});
+
+    return await getPostsList();
 }
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,70 @@
 import {useState} from 'react';
-import {createPost, getPostsList} from "../gateway/postsGateway";
+import {changePost, createPost, deletePost, getPostsList} from "../gateway/postsGateway";
 import MainLayout from "../components/MainLayout";
 
 
 export default function Home({posts}) {
     const [postList, setPostList] = useState(posts);
-    const [text, setText] = useState('');
+    const [textCreate, setTextCreate] = useState('');
+    const [textEdit, setTextEdit] = useState('');
+    const [visibilityForm, setVisibilityForm] = useState(false);
+    const [postId, setPostID] = useState(null);
 
     const handleChange = (e) => {
-        setText(e.target.value);
+        const formCreate = document.querySelector('.form-posts').contains(e.target);
+
+        formCreate ? setTextCreate(e.target.value) : setTextEdit(e.target.value);
     }
+
 
     const handleSumbit = async (e) => {
         e.preventDefault();
 
-        if (!text) return null;
+        if (!textCreate) return null;
 
-        await createPost(text);
-        setText('');
-
-        const newPostList = await getPostsList();
-
-        setPostList(newPostList.data);
-
+        setPostList(await createPost(textCreate));
+        setTextCreate('');
     }
 
-    const handleToggleClass = (e) => {
-        e.target.classList.toggle('hidden')
+    const handleDeletePost = async (id) => setPostList(await deletePost(id));
+
+
+    const handleChangePost = async (e, id) => {
+        e.preventDefault();
+
+        setPostList(await changePost(id, `${textEdit}`));
+        handleToggleVisability(id);
+    };
+
+
+    const handleToggleVisability = (id, text) => {
+        setTextEdit(text);
+        setPostID(id);
+        setVisibilityForm(!visibilityForm);
     }
+
 
     return (
         <MainLayout>
             <form className="form-posts" onSubmit={handleSumbit}>
-                <input type="text" className="form-posts__text" onChange={handleChange} value={text}
+                <input type="text" className="form-posts__text" onChange={handleChange} value={textCreate}
                        placeholder="What's happening?"/>
                 <input type="submit" value="Tweet" className="form-posts__btn"/>
             </form>
             <ul className="posts">
-                {postList.map(({text,created_at,id}) => (
+                {postList.map(({text, created_at, id}) => (
                     <li key={id} className="posts__item">
                         <div className="posts__user">
                             <span className="posts__name">User name</span>
                             <span className="posts__date">{new Date(created_at).getFullYear()}</span>
-                            <div className="posts__edit hidden" onClick={handleToggleClass}>
+                            <div className="posts__edit">
                                 <ul className="post-menu">
-                                    <li className="post-menu__item"><i className="fas fa-trash-alt"/> <span
+                                    <li className="post-menu__item" onClick={() => handleDeletePost(id)}><i
+                                        className="fas fa-trash-alt"/> <span
                                         className="post-menu__del">Delete</span></li>
-                                    <li className="post-menu__item"><i className="fas fa-pencil-alt"/> <span
-                                        className="post-menu__text">Edt</span></li>
+                                    <li className="post-menu__item " onClick={() => handleToggleVisability(id, text)}>
+                                        <i className="fas fa-pencil-alt"/> <span
+                                        className="post-menu__text">Edit</span></li>
                                 </ul>
                             </div>
                         </div>
@@ -55,12 +72,16 @@ export default function Home({posts}) {
                     </li>
                 ))}
             </ul>
+            {visibilityForm &&
+            <form className="form-edit" onSubmit={(e) => handleChangePost(e, postId)}>
+                <input type="text" defaultValue={textEdit} className="form-edit__text" onChange={handleChange}/>
+                <input type="submit" value="Change" className="form-edit__btn"/>
+            </form>}
         </MainLayout>
     )
 }
 
 Home.getInitialProps = async () => {
-    const postsList = await getPostsList();
+    return {posts: await getPostsList()}
+};
 
-    return {posts: postsList.data}
-}
