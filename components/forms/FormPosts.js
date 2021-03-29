0aaea48bfc73783f39f6f65744a9bd9fafdc9ab4
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-03-29 15:00:45 +0300

    added label for input

--- components/forms/FormPosts.js
+++ components/forms/FormPosts.js
@@ -5,7 +5,7 @@ export default function FormPosts({onSubmit,post}) {
     const [content, setContent] = useState(post.content );
     const [title, setTitle] = useState(post.title );
 
-    const handleInputChange = (e) => e.target.name === "title" ? setTitle(e.target.value) : setContent(e.target.value);
+    const handleInputChange = (e) => e.target.id === "title" ? setTitle(e.target.value) : setContent(e.target.value);
 
 
     const handleCreateSubmit = e => {
@@ -27,9 +27,11 @@ export default function FormPosts({onSubmit,post}) {
 
     return (
         <form className="form-posts" onSubmit={handleCreateSubmit}>
-            <input type="text" name='title' className="form-posts__title" onChange={handleInputChange} value={title}
+            <label htmlFor="title" className='form-posts__label'>Title</label>
+            <input type="text" id='title' className="form-posts__title" onChange={handleInputChange} value={title}
                    placeholder="Title?"/>
-            <input type="text" name='content' className="form-posts__text" onChange={handleInputChange} value={content}
+            <label htmlFor="content" className='form-posts__label'>Content</label>
+            <input type="text" id='content' className="form-posts__text" onChange={handleInputChange} value={content}
                    placeholder="What's happening?"/>
             <input type="submit" value="Tweet" className="form-posts__btn"/>
         </form>
