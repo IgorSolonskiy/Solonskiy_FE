5041b513a0e80969864cc7b31057f187070b6b96
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-03-30 14:53:24 +0300

    destructure post obj

--- components/forms/FormPosts.js
+++ components/forms/FormPosts.js
@@ -2,21 +2,21 @@ import {useState} from 'react';
 
 export default function FormPosts({onSubmit, post = {content: '', title: ''}}) {
     const [posts, setPosts] = useState(post);
+    const {title, content} = posts;
 
-    const handleChange = (e) => {
-        setPosts(prev => {
-            return {
-                ...prev,
-                [e.target.id]: e.target.value,
-            }
-        })
-    }
+    const handleInputChange = (e) => setPosts(prev => {
+        return {
+            ...prev,
+            [e.target.id]: e.target.value,
+        }
+    })
 
-    const handleCreateSubmit =async e => {
+
+    const handleCreateSubmit = async e => {
         e.preventDefault();
 
-        if(!posts.content || !posts.title){
-            return ;
+        if (!content || !title) {
+            return;
         }
 
         await onSubmit(posts);
@@ -26,10 +26,10 @@ export default function FormPosts({onSubmit, post = {content: '', title: ''}}) {
     return (
         <form className="form-posts" onSubmit={handleCreateSubmit}>
             <label htmlFor="title" className='form-posts__label'>Title</label>
-            <input type="text" id='title' className="form-posts__title" onChange={handleChange} value={posts.title}
+            <input type="text" id='title' className="form-posts__title" onChange={handleInputChange} value={title}
                    placeholder="Title?"/>
             <label htmlFor="content" className='form-posts__label'>Content</label>
-            <input type="text" id='content' className="form-posts__text" onChange={handleChange} value={posts.content}
+            <input type="text" id='content' className="form-posts__text" onChange={handleInputChange} value={content}
                    placeholder="What's happening?"/>
             <input type="submit" value="Tweet" className="form-posts__btn"/>
         </form>
