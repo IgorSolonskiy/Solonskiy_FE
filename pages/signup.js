d3aac45c105de19cb776f06e277cfe3c68060aae
IgorSolonskiy <solonskiyigor@gmail.com>
2021-04-15 14:24:35 +0300

    created signup page and signup form

---
+++ pages/signup.js
@@ -0,0 +1,62 @@
+import {useState} from 'react';
+import {useRouter} from "next/router";
+import {confirmUser, registerUser} from "../gateway/usersGateway";
+
+import MainLayout from "../components/layout/MainLayout";
+import FormSignup from "../components/forms/FormSignup";
+import Link from "next/link";
+
+export default function Signup() {
+    const router = useRouter();
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChangeInput = (key,value) =>{
+        setUser(prevUser=>{
+            return {
+                ...prevUser,
+                [key]: value
+            }
+        })
+    }
+
+    const handleSubmitForm = async (e,user) =>{
+        e.preventDefault();
+
+        const token = await registerUser(user);
+
+        document.cookie = `jwt=${JSON.stringify(token)}`;
+        router.push('/');
+    }
+
+    return (
+        <MainLayout>
+            <div className='h-100 d-flex flex-column justify-content-center'>
+                <FormSignup onChange={handleChangeInput}
+                            onSubmit={handleSubmitForm}
+                            user={user}/>
+                <Link href="/login"><span className='btn btn-primary mt-2'>Log in</span></Link>
+            </div>
+        </MainLayout>
+    )
+}
+
+export async function getServerSideProps(context) {
+    try {
+        await confirmUser(context);
+
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    } catch (e) {
+        return {
+            props: {},
+        }
+    }
+}
