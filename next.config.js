f7ed0dd663e2e639142110c01c81f1d72525b44b
solonskiy <solonskiyigor@gmail.com>
2021-04-30 15:14:59 +0300

    changed next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         API_URL: process.env.BASE_URL_API,
-        AUTH_TOKEN_LIFETIME: 60*60*24,
+        AUTH_TOKEN_LIFETIME: process.env.AUTH_TOKEN_LIFETIME,
     },
 }
