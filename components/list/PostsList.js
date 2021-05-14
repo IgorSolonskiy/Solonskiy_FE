504ec50882b6369c17b2c7ae9380509ca6a78957
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-05-14 11:34:32 +0300

    changed style postsList component

--- components/list/PostsList.js
+++ components/list/PostsList.js
@@ -5,7 +5,7 @@ export default function PostsList({onDelete}) {
     const {posts} = useSelector((state) => state.posts)
 
     return (
-        <ul className="list-group list-group-flush w-100 vh-100 px-3 mb-3 border-top" style={{overflowY: "scroll"}}>
+        <ul className="list-group list-group-flush w-100 vh-100 px-3 mb-3 mt-3 border-top" style={{overflowY: "scroll"}}>
             {posts.map(post => <Post key={post.id} post={post} onDelete={onDelete}/>)}
         </ul>
     )
