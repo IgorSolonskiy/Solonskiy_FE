ed616be62f2774d24eef0c116f58c4a941dc8383
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-05-14 14:07:51 +0300

    changed style

--- components/list/UsersList.js
+++ components/list/UsersList.js
@@ -4,7 +4,7 @@ import UserInfo from "../user/UserInfo";
 export default function UsersList() {
     const {users} = useSelector((state) => state.users)
     return (
-        <ul className='list-group position-absolute min-vh-70 ' style={{zIndex: 30}}>
+        <ul className='list-group position-absolute h-50 overflow-hidden' style={{zIndex: 30}}>
             {users.map(user => <UserInfo user={user} key={user.id}/>)}
         </ul>
     )
