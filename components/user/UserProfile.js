f2147bad2f3de12aa95548f160344173378f0780
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-05-24 11:27:51 +0300

    changed userProfile

--- components/user/UserProfile.js
+++ components/user/UserProfile.js
@@ -7,16 +7,15 @@ export default function UserProfile() {
     const {profile} = useSelector(state => state.profile);
     const {user} = useSelector(state => state.users);
     const {post} = useSelector(state => state.posts);
+    const userName = user ? user.name : profile.name;
+    const userAvatar = user ? user.avatar : profile.avatar;
+    const userLogin = user ? user.username : profile.username;
 
     const returnBackLink = user && post ?
         <Link href={`/users/${user.username}`}>
             <span className='btn btn-outline-secondary my-3'>&lArr;</span>
         </Link>
         : null;
-
-        const userAvatar = user ? user.avatar : profile.avatar;
-        const userName = user ? user.name : profile.name;
-        const userLogin = user ? user.username : profile.username;
 
     return (
         <div className="d-flex align-items-center justify-content-around w-100 mt-3 mb-3">
