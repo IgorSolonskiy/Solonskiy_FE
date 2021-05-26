75e70dbc84c7bbdc669ee32ecf3b9ef4b56d84b1
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-05-26 13:03:03 +0300

    change env and redux posts

--- store/user/actions.js
+++ store/user/actions.js
@@ -9,9 +9,9 @@ export const usersActionTypes = {
 export const setPaginateUsersData = (payload) => ({ type: usersActionTypes.SET_PAGINATE_USERS_DATA, payload });
 export const setUser = (payload) => ({ type: usersActionTypes.SET_USER, payload });
 
-export const setSearchUsersListAsync = (username) => async dispatch => {
+export const setSearchUsersListAsync = (username, page = 1) => async dispatch => {
   const { data: response } = await apiClient
-    .get(`users?username=${username}&limit=${process.env.NUMBER_TOTAL_SEARCH_USERS}`);
+    .get(`users?username=${username}&limit=${process.env.NUMBER_TOTAL_PAGINATE_USERS}&page=${page}`);
 
   dispatch(setPaginateUsersData(response));
 };
