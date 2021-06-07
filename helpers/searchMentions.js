a7d50b52b771a5ea4024e740048e7c4b85bf63e3
IgorSolonskiy <https://github.com/IgorSolonskiy>
2021-06-07 10:12:19 +0300

    changed helpers func

--- helpers/searchMentions.js
+++ helpers/searchMentions.js
@@ -1,20 +1,18 @@
-import {getSearchUsers} from '../api/users';
-import {getSearchHashtags} from '../api/tags';
+import { getSearchUsers } from "../api/users";
+import { getSearchHashtags } from "../api/tags";
 
 export const searchMentions = async (search, prefix, formik) => {
-  await formik.setFieldValue('searchData', []);
-  await formik.setFieldValue('search', search);
-  await formik.setFieldValue('loading', !!formik.values.search);
+  formik.setFieldValue("search", search);
+  formik.setFieldValue("loading", !!formik.values.search);
 
   if (search.length > 1 && !formik.values.searchData.length) {
-    await formik.setFieldValue('loading', false);
-    return formik.setFieldValue('searchData', []);
+    return formik.setFieldValue("loading", false);
   }
 
-  const searchData = prefix === '@'
-      ? await getSearchUsers(search)
-      : await getSearchHashtags(search);
+  const searchData = prefix === "@"
+    ? await getSearchUsers(search)
+    : await getSearchHashtags(search);
 
-  await formik.setFieldValue('searchData', searchData);
-  await formik.setFieldValue('loading', false);
+  formik.setFieldValue("searchData", searchData);
+  formik.setFieldValue("loading", false);
 };
